63be4d6dcb0b21c4f8501af40c8461e35322934b
Mingzhe <57017232+SuperbTUM@users.noreply.github.com>
2020-12-31 17:22:16 +0800

    Update template_recursion.py

--- template_recursion.py
+++ template_recursion.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def solution(n: int, arr: List[str]): -> List[str]
+def solution(n: int, arr: List[str]): -> List[List[str]]
     def template_recursion(start: int, path: List[str], result: List[str], n: int, arr: List[str]):
         if len(path) == n:
             result.append(path[:])
@@ -13,3 +13,20 @@ def solution(n: int, arr: List[str]): -> List[str]
     result = []
     template_recursion(0, [], result, n, arr)
     return result
+
+def solution2(arr: List[int]): -> List[List[int]]
+    def template_recursion(path: List[int], result: List[int], visited: List[int], arr: List[int]):
+        if len(path) == len(arr):
+            result.append(path[:])
+            return
+        for i in range(len(arr)):
+            if not visited[i]:
+                path.append(arr[i])
+                visited[i] = True
+                template_recursion(i+1, path, result, n, arr)
+                path.pop()
+                visited[i] = False
+    result = []
+    visited = [False] * len(arr)
+    template_recursion([], result, visited, arr)
+    return result    
