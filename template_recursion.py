1bb7bf2a93e97fa336110b58f3230c91aa79e958
Mingzhe <57017232+SuperbTUM@users.noreply.github.com>
2021-01-01 21:11:04 +0800

    Update template_recursion.py

--- template_recursion.py
+++ template_recursion.py
@@ -1,12 +1,13 @@
 from typing import List
 
+global judgement
 def solution(n: int, arr: List[str]): -> List[List[str]]
     def template_recursion(start: int, path: List[str], result: List[str], n: int, arr: List[str]):
         if len(path) == n:
             result.append(path[:])
             return
         for i in range(start, len(arr)):
-            if judgement:
+            if judgement: # The judgement is for pruning
                 path.append(arr[i])
                 template_recursion(i+1, path, result, n, arr)
                 path.pop()
@@ -20,7 +21,7 @@ def solution2(arr: List[int]): -> List[List[int]]
             result.append(path[:])
             return
         for i in range(len(arr)):
-            if not visited[i]:
+            if not visited[i] and judgement:
                 path.append(arr[i])
                 visited[i] = True
                 template_recursion(i+1, path, result, n, arr)
@@ -30,3 +31,9 @@ def solution2(arr: List[int]): -> List[List[int]]
     visited = [False] * len(arr)
     template_recursion([], result, visited, arr)
     return result    
+
+# example for judgement
+# for subset
+judgement = ["i > start and nums[i] == nums[i-1]"]
+# for full array
+judgement = ["i > 0 and nums[i] == nums[i-1] and not visited[i]"]
