01945a7bc104cf9e757cde8ed82ab3de07abb4c9
Mingzhe <57017232+SuperbTUM@users.noreply.github.com>
2021-01-12 18:54:25 +0800

    Add files via upload

--- adjunct-table.py
+++ adjunct-table.py
@@ -24,7 +24,7 @@ class AdjacencyTable:
         cnt = 0
         while stack:
             node = stack.pop(0)
-            # node = stack.pop() is slower
+            # node = stack.pop() is slower because a queue is faster
             cnt += 1
             for cont in self.adj[node]:
                 # one effective rear node found
@@ -33,3 +33,41 @@ class AdjacencyTable:
                 if self.in_degrees[cont] == 0:
                     stack.append(node)
         return self.n == cnt
+
+
+# The core idea of DFS is to judge if a circle exists inside the adjunct table
+# Not sure if an inverse one is better
+class AdjacencyTableDFS:
+    def __init__(self, n):
+        self.adj = [set() for _ in range(n)]
+        self.in_degrees = [0] * n
+        self.n = n
+        self.visited = [0] * n  # three status : 0 -- not visited; 1 -- visited and traverse completed;
+        # 2 -- visited and traverse not completed, which means a circle exists
+
+    def build(self, example):
+        for first, second in example:
+            # assuming first -> second
+            self.adj[first].add(second)
+            # The rear node needs one more pre-condition (precursor node) to be found
+            self.in_degrees[second] += 1
+
+    def find_circle(self):
+        for i in range(len(self.adj)):
+            if self._dfs(i, self.adj, self.visited):
+                return False
+            return True
+
+    def _dfs(self, i, adj, visited):
+        # stop condition
+        if visited[i] == 2:
+            return True
+        if visited[i] == 1:
+            return False
+        visited[i] = 2
+        for cont in adj[i]:
+            if self._dfs(cont, adj, visited):
+                return True
+        visited[i] = 1
+        return False
+
